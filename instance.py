805784c22b79340c8ab2458608f6e1281da36d25
PinePunk16 <106622968+PinePunk16@users.noreply.github.com>
2023-05-12 11:26:08 +0200

    Add files via upload
    Removed example files. Proper documentation to come.

---
+++ instance.py
@@ -0,0 +1,90 @@
+# instance.py
+# Class to be inherited by primary objects.
+
+import bson
+import os
+import random
+import string
+
+from pyprimary.parser import parse_pool
+
+
+
+class Instance:
+    # Generates an ID and saves the desired folder. If not given it's assumed as project root
+    def __init__(self, directory = "") -> None:
+        self._directory_: str = directory
+        self.id_: str = ""
+
+        if not os.path.exists(self._directory_):
+            os.mkdir(self._directory_)
+
+        while True:
+            self.id_ = "".join(random.choices(string.ascii_letters + string.digits, k=12))
+            if self.id_ not in os.listdir(self._directory_):
+                break
+    # Saves to BSON. Ignores attributes that start with "_"
+    def save(self) -> None:
+        data: dict = {}
+        for key, value in self.__dict__.items():
+            if not key.startswith("_"):
+                if not isinstance(value, (int, float, str, bool, dict, list, tuple)):
+                    value = str(value)
+                data[key] = value
+        
+        try:
+            if not os.path.isdir(self._directory_):
+                os.mkdir(self._directory_)
+            
+            with open(os.path.join(self._directory_, f"{self.id_}.bson"), "wb") as bson_file:
+                bson_file.write(bson.dumps(data))
+        except Exception as exception:
+            print(f"ERROR: {exception}")
+            return None       
+    # Loads from BSON. Sets attributes that start with "_" to None, except for saving and loading directory
+    def load(self) -> None:
+        data: dict = {}
+        try:
+            with open(os.path.join(self._directory_, f"{self.id_}.bson"), "rb") as bson_file:
+                data = bson.loads(bson_file.read())
+        except Exception as exception:
+            print(f"ERROR: {exception}")
+            return None
+        
+        for key, value in data.items():
+            if type(getattr(self, key)) == tuple:
+                setattr(self, key, tuple(value))
+            elif type(getattr(self, key)) == set:
+                setattr(self, key, set(value))
+            else:
+                setattr(self, key, value)
+            
+        for key, value in self.__dict__.items():
+            if key not in data and key != "_directory_":
+                setattr(self, key, None)
+    # Shows the value of all attributes
+    def show(self) -> None:
+        print("\t".join([f"{key}: {value}" for key, value in self.__dict__.items()]))
+    # Loads a random line from a pool file and gives it to the parameter with name passed as string, converting it to its type in the process
+    # The pool file for any parameter is "<directory>/pools/<parameter name>.pool"
+    def generate_parameter(self, parameter: str) -> None:
+        try:
+            directory_path: str = os.path.join(self._directory_, "pools")
+            if not os.path.exists(directory_path):
+                os.makedirs(directory_path)
+            
+            file_path: str = os.path.join(directory_path, f"{parameter}.pool")
+            if not os.path.isfile(file_path):
+                open(file_path, 'w').close()
+            
+            value: any = parse_pool(file_path = file_path)
+            if value is not None:
+                value = type(getattr(self, parameter))(value)
+            setattr(self, parameter, value)
+        except Exception as exception:
+            print(f"ERROR: {exception}")
+    # Calls the function "generate_parameter" for all parameters, except parameters ending in "_"
+    def generate(self) -> None:
+        for key, value in self.__dict__.items():
+            if not key.endswith("_"):
+                self.generate_parameter(key)
---
+++ parser.py
@@ -0,0 +1,107 @@
+# parser.py
+# For parsing of pool files.
+
+import random
+
+
+
+"""
+Picks a random line from a file. If the line is a percentage (e.g. "50%"), the function will pick a random line 
+from the lines before the next percentage (or the end of the file) with the specified probability. 
+
+There must be at least one percentage, and the percentages must add up to 100, otherwise the function will return None. 
+To define a None value, make the percentage section it's in empty.
+
+To select a random integer in a range, use the syntax "random[a, b)" for an exclusive range or "random[a, b]" for an inclusive range.
+
+Example 1:
+
+----------------
+50%
+    4
+    2
+25%
+25%
+    random[0, 10)
+-----------------
+
+In this example, the function will generate either 4 or 2 with a 50% chance. 
+With a 25% chance, it will return None. 
+With another 25% chance, it will generate a random value between 0 (inclusive) and 10 (inclusive).
+
+Example 2:
+----------------
+99.99%
+    Martha
+    John
+    Alan
+    Jake
+    Alex
+0.01%
+    Ashlathon the conquerer of worlds
+-----------------
+
+In this example, the function will generate with a high change a name between "Martha", "John", "Alan", "Jake" and "Alex".
+With a very low chance, the name will be "Ashlathon the conquerer of worlds".
+"""
+
+def parse_pool(file_path: str) -> str:
+    lines: list[str] = []
+    
+    try:
+        with open(file_path, "r") as file:
+            for line in file:
+                lines.append(line.strip())
+    except Exception as exception:
+        print(f"ERROR: {exception}")
+    
+    percentages: list[float] = []
+    non_percentages: list[list[str]] = [[]]
+    percentages_index: int = 0
+    
+    for line in lines:
+        if line.endswith("%"):
+            percentages_index += 1
+            non_percentages.append([])
+            
+            percentages.append(float(line[:-1]))
+        else:
+            non_percentages[percentages_index].append(line)
+    non_percentages.pop(0)
+    
+    result_list: list[str] = None
+    
+    if not non_percentages:
+        return None
+    
+    if(percentages):
+        total_percentage = sum(percentages)
+        if(total_percentage != 100):
+            return None
+        
+        random_number = random.uniform(0, 100)
+        cumulative_percentage = 0
+        for index, percentage in enumerate(percentages):
+            cumulative_percentage += percentage
+            if random_number <= (cumulative_percentage / total_percentage * 100) or index == len(percentages) - 1:
+                result_list = non_percentages[index]
+                break
+    else:
+        return None
+    
+    if result_list:
+        result: str = random.choice(result_list)
+        
+        if (result.startswith("random(") or result.startswith("random[")) and (result.endswith(")") or result.endswith("]")):
+            values: list[float] = result[7:-1].split(",")
+            min_value: int = int(values[0])
+            max_value: int = int(values[1])
+
+            left_inclusive: bool = result[6] == "["
+            right_inclusive: bool  = result[-2] == "]"
+            
+            return str(random.randint(min_value if left_inclusive else min_value + 1, max_value if right_inclusive else max_value - 1))
+        else:
+            return result
+    else:
+        return None
